18ac80e0c3e8c334b58cd6486bc93cd6b44df075
ahbpp <ahbpp@hotmail.com>
2017-11-29 17:40:01 +0300

    Update solve.go

--- task5/solve.go
+++ task5/solve.go
@@ -23,7 +23,7 @@ var(
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", AddURL).Methods("POST")
-	r.HandleFunc("/{key}", GetURL)/*.Methods("GET")*/ //Фигурные скобки означают переменную
+	r.HandleFunc("/{key}", GetURL)
 	http.ListenAndServe(":8082", r)
 }
 
@@ -31,11 +31,11 @@ func AddURL(w http.ResponseWriter, r *http.Request) {
 	//r - переданный запрос
 	//w - ответ
 	var MyLongUrl LongUrl
-	json.NewDecoder(r.Body).Decode(&MyLongUrl) // записали в itemAdd информацию из json-строки
+	json.NewDecoder(r.Body).Decode(&MyLongUrl) 
 
 	var MyShortUrl ShortUrl
 	MyShortUrl.Key = strconv.Itoa(keyId)
-	URLStore[keyId] = MyLongUrl.Url //записали URL в мэпу
+	URLStore[keyId] = MyLongUrl.Url 
 	keyId += 1
 
 	json.NewEncoder(w).Encode(&MyShortUrl)
